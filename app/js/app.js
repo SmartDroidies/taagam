edc4ff52c23825427c58a5713163aa9e00687b39
SmartDroidies <smartdroidies@gmail.com>
2014-09-08 22:59:17 +0530

    Listing enhanced

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,44 @@
 'use strict';
-
 /* App Module */
-
 var taagamApp = angular.module('taagamApp', [
   'ngRoute',
   'taagamControllers'
 ]);
 
+
+// Response Interceptor 
+/*
+taagamApp.config(['$httpProvider', function ($httpProvider) {
+    var $http,
+        interceptor = ['$q', '$injector', function ($q, $injector) {
+            var error;
+
+            function success(response) {
+                // get $http via $injector because of circular dependency problem
+                $http = $http || $injector.get('$http');
+                if($http.pendingRequests.length < 1) {
+                    $('#loadingWidget').hide();
+                }
+                return response;
+            }
+
+            function error(response) {
+                // get $http via $injector because of circular dependency problem
+                $http = $http || $injector.get('$http');
+                if($http.pendingRequests.length < 1) {
+                    $('#loadingWidget').hide();
+                }
+                return $q.reject(response);
+            }
+
+            return function (promise) {
+                $('#loadingWidget').show();
+                return promise.then(success, error);
+            }
+        }];
+    $httpProvider.responseInterceptors.push(interceptor);
+}]);
+*/
 taagamApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,17 +6,31 @@ var taagamControllers = angular.module('taagamControllers', []);
 
 taagamControllers.controller('CtgryListCtrl', ['$scope', '$http',
   function($scope, $http) {
+	  $('#loadingWidget').show();
 	  $http.get('http://smartdroidies.com/api/thuligal.php?service=ctgry').success(function(data) {
 		$scope.categories = data;
-	  });
+		$('#loadingWidget').hide();
+		$('#popup-error').hide();
+	  }).error(function(data, status) {
+		$('#loadingWidget').hide();
+		$('#popup-content').html(jQuery.i18n.prop("error.network"));
+		$('#popup-error').popup( "open");
+	 });
   }]);
 
 taagamControllers.controller('CtgryItemsCtrl', ['$scope', '$routeParams', '$http',
   function($scope, $routeParams, $http) {
+	$('#loadingWidget').show();
 	$http.get('http://smartdroidies.com/api/thuligal.php?service=' + $routeParams.category).success(function(data) {
       $scope.items = data;
 	  $scope.category = $routeParams.category;
-    });
+      $('#loadingWidget').hide();
+	  $('#popup-error').hide();
+    }).error(function(data, status) {
+		$('#loadingWidget').hide();
+		$('#popup-content').html(jQuery.i18n.prop("error.network"));
+		$('#popup-error').popup( "open");
+	});
   }]);
 
 taagamControllers.controller('ItemDetailCtrl', ['$scope', '$routeParams', '$http',
